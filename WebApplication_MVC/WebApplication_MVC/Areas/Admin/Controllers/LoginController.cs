1d64588bd6bafcd2252a962c5d05819d8fbc735b
jdai <nguyendai4713@gmail.com>
2024-06-11 08:04:32 +0700

    update

---
+++ WebApplication_MVC/WebApplication_MVC/Areas/Admin/Controllers/LoginController.cs
@@ -0,0 +1,37 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication_MVC.Areas.Admin.Code;
+using WebApplication_MVC.Areas.Admin.Models;
+
+namespace WebApplication_MVC.Areas.Admin.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Admin/Login
+        [HttpGet]//nhận từ url
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(LoginModels model)
+        {
+            var result = new AccountModels().Login(model.UserName, model.Password);
+            if (result && ModelState.IsValid)
+            {
+                SessionHelper.SetSession(new UserSession() { UserName = model.UserName });
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ModelState.AddModelError("", "Tên đăng nhập không chính xác");
+            }
+            return View(model);
+        }
+    }
+}
---
+++ WebApplication_MVC/WebApplication_MVC/Areas/Admin/Models/LoginModels.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication_MVC.Areas.Admin.Models
+{
+    public class LoginModels
+    {
+        [Required]//bắt buộc
+        public string UserName { get; set;}
+        public string Password { get; set;}
+        public string RememberMe { get; set;}
+    }
+}
